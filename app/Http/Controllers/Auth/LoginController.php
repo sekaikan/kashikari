577e76636549c77af0fba412b29721603e4b5687
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-05 05:43:37 +0000

    username & password login implemented!

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    
+    public function username()
+    {
+        return 'name';
+    }
+     
+    protected function attemptLogin(Request $request)
+    {
+        $username = $request->input($this->username());
+        $password = $request->input('password');
+     
+        $credentials = [$this->username() => $username, 'password' => $password];
+     
+        return $this->guard()->attempt($credentials, $request->filled('remember'));
     }
 }
