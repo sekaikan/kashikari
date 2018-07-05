e890b3cec9ea8e1a7d7d7e3173f0d11fe466b5da
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-05 05:35:53 +0000

    email form on register page fixed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,7 +30,7 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
---}}
+
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -38,7 +38,7 @@
                                 @endif
                             </div>
                         </div>
-
+--}}
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
