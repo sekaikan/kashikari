292b9fccaac7ed45bc325f41cfbca779f2d82c47
saaya.akashi <ec2-user@ip-172-31-17-77.us-east-2.compute.internal>
2018-07-05 06:30:37 +0000

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,95 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+@section('cover')
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="cover">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="/images/home1.jpg" alt="First slide">
             </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="/images/home2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="/images/home3.jpg" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
     </div>
+@endsection
+@section('content')
+    
+    <div class="row">
+        <div class="col-4">
+            <button type="button" class="btn btn-primary btn-lg btn-block">グループを作る</button>
+        </div>
+        <div class="col-4">
+            <button type="button" class="btn btn-primary btn-lg btn-block">グループを探す</button>
+        </div>
+        <div class="card col-4">
+          <div class="card-header">
+            Group List
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="/group/home" >18th New Grads</a></li>
+            <li class="list-group-item">Sekaikan</li>
+            <li class="list-group-item">6A</li>
+            <li class="list-group-item">8A</li>
+            <li class="list-group-item">Eco/Cafeteria comittee</li>
+            <li class="list-group-item">Attendance comittee</li>
+          </ul>
+        </div>
+    </div>
+    <div class="row">
+        <h3>グループ　ランキング</h3>
+    </div>
+    <div class="row">
+        <div class="card col-3">
+          <div class="card-header">
+            １位
+          </div>
+          <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">18th New Grads</p>
+          </div>
+        </div>
+        <div class="card col-3">
+          <div class="card-header">
+            ２位
+          </div>
+          <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Sekaikan</p>
+          </div>
+        </div>
+        <div class="card col-3">
+          <div class="card-header">
+            ３位
+          </div>
+          <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Eco/Cafeteria comittee</p>
+          </div>
+        </div>
+        <div class="card col-3">
+          <div class="card-header">
+            ４位
+          </div>
+          <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Attendance comittee</p>
+          </div>
+        </div>
+     </div>
 </div>
+
 @endsection
