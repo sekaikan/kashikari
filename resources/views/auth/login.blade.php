b61c16e180ea3086924aa1df6da87c8a1a9d3588
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-05 05:33:22 +0000

    email form on login blade disabled

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
-
+{{--
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
@@ -24,7 +24,7 @@
                                 @endif
                             </div>
                         </div>
-
+--}}
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
