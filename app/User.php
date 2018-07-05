eb309af30079f3691fdaee17696249aa794e1128
saaya.akashi <ec2-user@ip-172-31-17-77.us-east-2.compute.internal>
2018-07-05 01:54:48 +0000

    user and item model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'content', 'photo',
     ];
 
     /**
@@ -26,4 +26,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function chats()
+    {
+        return $this->hasMany(Chat::class)->paginate(100);
+    }
+    
+    public function posts()
+    {
+        return $this->hasMany(Post::class)->paginate(20);
+    }
+    
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+    
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class)->withTimestamps();
+    }
 }
