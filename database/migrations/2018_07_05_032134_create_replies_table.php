b20e548b3aab35aff3152675d484427dc1a16dfa
JunWoo Choi <ec2-user@ip-172-31-11-104.us-east-2.compute.internal>
2018-07-05 03:24:15 +0000

    replies_migration

---
+++ database/migrations/2018_07_05_032134_create_replies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('post_id')->unsigned()->index();
+            $table->text('content');
+            $table->string('status')->index()->nullable();
+            $table->timestamps();
+            
+            
+            
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
