0ac52aaf3540380b51dbc3df22bf58633cac82e0
erika.tokumoto <>
2018-07-06 01:38:56 +0000

    items.blade 1

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,20 +1,33 @@
-<ul class="media-list">
+@if ($items)
+    <div class="row">
 @foreach ($items as $item)
     <?php $user = $item->user; ?>
-    <li class="media">
-        <div class="media-body">
+       <div class= "col-xl-3 col-md-4 col-6">
             <div>
-                {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $item->created_at }}</span>
+                {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">{{ $item->date }}</span>
             </div>
-            <div>
-                <p>{!! nl2br(e($item->name)) !!}</p>
-                <p>{!! nl2br(e($item->content)) !!}</p>
-                <p>{!! nl2br(e($item->status)) !!}</p>
-                <p>{!! nl2br(e($item->reward)) !!}</p>
-                <p>{!! nl2br(e($item->photo)) !!}</p>
-            </div>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="{!! nl2br(e($item->photo)) !!}" alt="items photo">
+                <div class="card-body">
+                    <h5 class="card-title">{!! nl2br(e($item->name)) !!}</h5>
+                    <p class="card-text">{!! nl2br(e($item->content)) !!}</p>
+                 <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Reward: {!! nl2br(e($item->reward)) !!}</li>
+                    <li class="list-group-item">{!! nl2br(e($item->status)) !!}</li>
+                 </ul>
+                  {!! link_to_route('items.show', "Detail", ['id' => $item->id]) !!}
+                </div>
+           </div>    
+            
+            
         </div>
     </li>
 @endforeach
 </ul>
 {!! $items->render() !!}
+@endif
+
+   
+              
+            
+    
