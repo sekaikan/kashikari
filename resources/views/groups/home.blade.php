01ca79d315c7e0b6173bd806e505ca7f5a333a34
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-06 04:54:17 +0000

    group home

--- resources/views/groups/home.blade.php
+++ resources/views/groups/home.blade.php
@@ -1,19 +1,15 @@
 @extends('layouts.app')
 
 @section('cover')
-    <div class="cover">
-        <div class="cover-inner">
-            <div class="cover-contents">
-                <h1> 18 New grads </h1>
-            </div>
-        </div>
+    <div class="jumbotron jumbotron-fluid">
+            <h1 class="text-center">18 New Grads</h1>
     </div>
 @endsection
 
 @section('content')
-<button type="button" class="btn btn-primary btn-lg btn-block"> <a href="/group/chat" class="btn btn-link text-info"><i class=""></i></a>chats</button>
+<a href="/group/chat" class="btn btn-primary btn-block">Chats</a>
 <a href="{{ route ('items.index') }}" class="btn btn-primary btn-block">貸すよお</a>
 <a href="{{ route ('posts.index') }}" class="btn btn-primary btn-block">貸して</a>
-<button type="button" class="btn btn-primary btn-lg btn-block"><a href="/group/edit" class="btn btn-link text-info"><i class=""></i></a>Group EDIT</button>
+<a href="/group/edit" class="btn btn-primary btn-block">Edit Groups</a>
 
 @endsection
