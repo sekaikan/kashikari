1d945e3793072ff57be9dff7c3dacb1e69ae80e9
erika.tokumoto <>
2018-07-06 04:46:28 +0000

    menu button

--- resources/views/groups/home.blade.php
+++ resources/views/groups/home.blade.php
@@ -12,7 +12,7 @@
 
 @section('content')
 <button type="button" class="btn btn-primary btn-lg btn-block"> <a href="/group/chat" class="btn btn-link text-info"><i class=""></i></a>chats</button>
-<button type="button" class="btn btn-primary btn-lg btn-block"><a href="/group/lend" class="btn btn-link text-info"><i class=""></i></a>貸すよ～</button>
+<a href="{{ route ('items.index') }}" class="btn btn-primary btn-block">貸すよお</a>
 <a href="{{ route ('posts.index') }}" class="btn btn-primary btn-block">貸して</a>
 <button type="button" class="btn btn-primary btn-lg btn-block"><a href="/group/edit" class="btn btn-link text-info"><i class=""></i></a>Group EDIT</button>
 
