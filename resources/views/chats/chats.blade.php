6f8c3785e5be9fc7f1a7253a312c41a1cbd880d2
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-09 03:51:19 +0000

    user post is now colored. Timezone test.

--- resources/views/chats/chats.blade.php
+++ resources/views/chats/chats.blade.php
@@ -1,11 +1,21 @@
 @foreach ($chats as $chat)
-<div class="card my-3">
-    <div class="card-body">
-        <p class="">{{ $chat->user->name }}</p>
-        <p class="display-4">{{ $chat->content }}</p>
-        <p class="small text-muted">Posted at {{ $chat->created_at }}</p>
-    </div>
-</div>
+    @if(Auth::id() == $chat->user_id)
+        <div class="card my-3 border-primary bg-light">
+            <div class="card-body">
+                <p class="text-muted">{{ $chat->user->name }}</p>
+                <p class="text-lead">{{ $chat->content }}</p>
+                <p class="small text-muted">Posted at {{ $chat->created_at }}</p>
+            </div>
+        </div>
+    @else
+        <div class="card my-3">
+            <div class="card-body">
+                <p class="text-muted">{{ $chat->user->name }}</p>
+                <p class="text-lead">{{ $chat->content }}</p>
+                <p class="small text-muted">Posted at {{ $chat->created_at }}</p>
+            </div>
+        </div>
+    @endif
 @endforeach
 
 {!! $chats->render() !!}
