c2750743ca1a650754687c7224afa7d221f6c7f2
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-09 06:01:49 +0000

    item edit update

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -1,47 +1,37 @@
 @extends('layouts.app')
 
-
-
 @section('content')
 
-<h1>アイテム編集ページ</h1>
-     {!! Form::model($item, ['route' => ['items.update', $item->id], 'method' => 'put']) !!}
+<div class="container">
+     {!! Form::model($item,['route' => ['items.update', $item->id], 'method' => 'put']) !!}
         <div class="row">
-            <div class="col-4" >
-                <div class="form-group">
-                    <form method="put" enctype="multipart/form-data">
-                    画像ファイル<input type="file" name="pic">
-                    <br>
-                    <input type="submit" name="btn" value="send">
-                    </form>
-                </div>
-             </div>   
-             
-             <div class="col-8">
+             <div class="col-md-6 offset-md-3">
+                 <h1 class="text-center font-weight-light">アイテム登録ページ</h1>
+                 <div class="text-center">
+                  <img src="{{ $item->photo }}" class="">
+                 </div>
                  <div class="form-group">
                     {!! Form::label('name', 'アイテム名') !!}
-                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                    {!! Form::text('name', $item->name, ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('content', '説明文') !!}
-                    {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+                    {!! Form::textarea('content', $item->content, ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('reward', '報酬') !!}
-                    {!! Form::text('reward', null, ['class' => 'form-control']) !!}
+                    {!! Form::text('reward', $item->reward, ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('status', 'Status') !!}
-                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                    {!! Form::text('status', $item->status, ['class' => 'form-control']) !!}
                 </div>
                 <button type="button" class="btn btn-success btn-lg">募集中</button>
                 <button type="button" class="btn btn-danger btn-lg">貸出中</button>
                 
               </div>
             {!! Form::submit('upload', ['class' => 'btn btn-primary btn-lg btn-block']) !!}
-              
             {!! Form::close() !!}
-            
         </div>
-    
+</div>
 @endsection
