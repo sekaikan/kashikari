601c9d71d3bb0f3883cde1df1cad24ddb1ec46f6
erika.tokumoto <>
2018-07-09 05:27:36 +0000

    home.blade edited

--- resources/views/groups/home.blade.php
+++ resources/views/groups/home.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.app')
 
 @section('cover')
-    <div class="jumbotron jumbotron-fluid">
-            <h1 class="text-center">18 New Grads</h1>
+    <div class="jumbotron jumbotron-fluid bg-dark">
+            <h1 class="text-center text-white">18 New Grads</h1>
     </div>
 @endsection
 
 @section('content')
+<div class="btn-group">
 <a href="/group/chats" class="btn btn-primary btn-block">Chats</a>
-<a href="{{ route ('items.index') }}" class="btn btn-primary btn-block">貸すよお</a>
-<a href="{{ route ('posts.index') }}" class="btn btn-primary btn-block">貸して</a>
 <a href="/group/edit" class="btn btn-primary btn-block">Edit Groups</a>
+</div>
+
+<div>
+<h2 class="text-center">Let's share!!</h2>
+@include('items.items', ['items' => $items])
+<a href="{{ route ('items.index') }}" class="">More...</a>
+</div>
+
+<div>
+<h2 class="text-center">Please borrow</h2>
+@include('posts.posts', ['posts' => $posts])
+<a href="{{ route ('posts.index') }}" class="">More...</a>
+</div>
+
 
 @endsection
