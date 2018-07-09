71ed0e3d862b255f802fe617786e22e85f06087a
erika.tokumoto <>
2018-07-09 04:37:47 +0000

    navbar left

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,7 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            
             <div class="navbar-header">
                 <a class="navbar-brand navbar-left" href="/home">Kashikari</a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +8,7 @@
                 </button>
             </div>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav navbar-right">
+                <ul class="navbar-nav ml-auto">
                     @if (Auth::check())
                          <li class="dropdown">
                                 <a href="#" class="dropdown-toggler" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
