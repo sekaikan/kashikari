7e71fda83e1afb9d28d28e63325101ec70854e3a
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-10 08:15:41 +0000

    button fixed

--- resources/views/replies/replies.blade.php
+++ resources/views/replies/replies.blade.php
@@ -48,31 +48,35 @@ foreach($replies as $reply) {
         @endfor
         <div class="col-md-{{ 12 - $spacer}}">
             <?php $user = $reply->user; ?>
-            <div class="card p-5">
-                <div class="row">
-                    <div class="col-md-2">
-                       <img src="{{ Gravatar::src($user->email, 30) . '&d=mm' }}" alt="" class="img-circle" style=" margin-right:10px; margin-top:25px;  border-radius: 20px;">
-                    </div>
-                    <div class="col-md-10">
-                        <div>
-                            {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $reply->created_at }}</span>
+            <div class="card p-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-2">
+                           <img src="{{ Gravatar::src($user->email, 30) . '&d=mm' }}" alt="" class="rounded-circle">
+                        </div>
+                        <div class="col-md-10">
+                            <div>
+                                {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $reply->created_at }}</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p>Reply ID: {{ $reply->id }}</p>
-                        <p>Replying to {{ $reply->reply_id }}.</p>
-                        <p>{!! nl2br(e($reply->content)) !!}</p>
-                        <p>{!! nl2br(e($reply->status)) !!}</p>
-                    </div>
-                    <div class="col-12">
-                        @if (Auth::id() == $reply->user_id)
-                            {!! Form::open(['route' => ['replies.destroy', $reply->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
-                            {!! Form::close() !!}
-                        @endif
-                        {!! link_to_route('replies.create','返信する!', ['reply_id' => $reply->id]) !!}
+                    <div class="row">
+                        <div class="col-12">
+                            <p>Reply ID: {{ $reply->id }}</p>
+                            <p>Replying to {{ $reply->reply_id }}.</p>
+                            <p>{!! nl2br(e($reply->content)) !!}</p>
+                            <p>{!! nl2br(e($reply->status)) !!}</p>
+                        </div>
+                        <div class="col-12">
+                            <div class="row justify-content-end">
+                                @if (Auth::id() == $reply->user_id)
+                                    {!! Form::open(['route' => ['replies.destroy', $reply->id], 'method' => 'delete']) !!}
+                                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                                    {!! Form::close() !!}
+                                @endif
+                                {!! link_to_route('replies.create','返信する!', ['reply_id' => $reply->id], ['class'=>'btn btn-primary']) !!}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
