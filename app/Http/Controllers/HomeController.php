5193f6a807c27672355ba415cfe72ed00ad1955b
JunWoo Choi <ec2-user@ip-172-31-11-104.us-east-2.compute.internal>
2018-07-12 05:25:28 +0000

    Group_revise

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,13 +25,13 @@ class HomeController extends Controller
          $user = \Auth::user();
          $follow_groups = \DB::table('groups')->join('group_user', 'groups.id', '=', 'group_user.group_id')->select('groups.*')->where('group_user.user_id', $user->id)->distinct()->paginate(20);
          $unfollow_groups = \DB::table('groups')->join('group_user', 'groups.id', '=', 'group_user.group_id')->select('groups.*')->where('group_user.user_id','!=', $user->id)->distinct()->paginate(20);
-         //$groups = Group::orderBy('updated_at', 'desc')->paginate(20);
+         $groups = Group::orderBy('updated_at', 'desc')->paginate(20);
       
         return view('home',[
             'unfollow_groups'=> $unfollow_groups,
             'user' => $user,
             'follow_groups' => $follow_groups,
-            //'groups' =>$groups,
+            'groups' =>$groups,
             ]);
     }
     
