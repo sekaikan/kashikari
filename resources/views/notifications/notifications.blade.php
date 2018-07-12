2df5c8b1e48daa01bb41512bbb881282c48dd254
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-12 05:25:30 +0000

    notifications can be deleted

--- resources/views/notifications/notifications.blade.php
+++ resources/views/notifications/notifications.blade.php
@@ -2,23 +2,36 @@
     <div class="card-header">
         Notifications <span class="badge badge-secondary">{{ Auth::user()->notifications()->count() }}</span>
     </div>
-    <ul class="list-group list-group-flush">
-        <?php $notifications = Auth::user()->notifications()->paginate(5); ?>
-        @foreach ($notifications as $notification)
-            <li class="list-group-item">
-                <div class="row">
-                    <div class="col-1">
-                        <img src="/images/user1.jpg" class="img-fluid rounded-circle"></img>
+    <?php $notifications = Auth::user()->notifications()->paginate(5); ?>
+    @if($notifications->count()==0)
+    <div class="card-body">
+        <p class="card-text text-muted"><i class="far fa-thumbs-up"></i> You have no new notifications.</p>
+    </div>
+    @else
+        <ul class="list-group list-group-flush">
+            @foreach ($notifications as $notification)
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-1">
+                            <img src="/images/user1.jpg" class="img-fluid rounded-circle"></img>
+                        </div>
+                        <div class="col-10">
+                            <small class="card-title"><span class="font-weight-bold">{{ App\User::find($notification->sender_id)->name }}</span> sent you a reply. <span class="badge badge-secondary">New</span></small>
+                            <p class="card-text text-muted">
+                                {{ $notification->content }}
+                            </p>
+                        </div>
+                        <div class="col-1">
+                            {!! Form::open(['route' => ['notifications.destroy', 'notification_id'=>$notification->id], 'method' => 'delete', 'class'=>'text-right']) !!}
+                                @if(Auth::id() == $notification->user_id)
+                                    {!! Form::button('<i class="far fa-trash-alt"></i>', ['type'=> 'submit', 'class' => 'btn btn-link text-secondary']) !!}
+                                @endif
+                            {!! Form::close() !!}
+                        </div>
                     </div>
-                    <div class="col-11">
-                        <small class="card-title"><span class="font-weight-bold">{{ App\User::find($notification->sender_id)->name }}</span> sent you a reply. <span class="badge badge-secondary">New</span></small>
-                        <p class="card-text text-muted">
-                            {{ $notification->content }}
-                        </p>
-                    </div>
-                </div>
-            </li>
-        @endforeach
-        {!! $notifications->render() !!}
-    </ul>
+                </li>
+            @endforeach
+            {!! $notifications->render() !!}
+        </ul>
+    @endif
 </div>
