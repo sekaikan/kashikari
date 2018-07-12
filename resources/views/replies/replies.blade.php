796462ed837502403143dd4ab79e89ed59886b0c
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-12 07:36:21 +0000

    reply form

--- resources/views/replies/replies.blade.php
+++ resources/views/replies/replies.blade.php
@@ -21,30 +21,50 @@ foreach($replies as $reply) {
             <div class="card p-4">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-2">
-                           <img src="{{ Gravatar::src($user->email, 30) . '&d=mm' }}" alt="" class="rounded-circle">
+                        <div class="col-md-1">
+                           <img src="{{ Gravatar::src($user->email, 1000) . '&d=mm' }}" alt="" class="rounded-circle img-fluid">
                         </div>
                         <div class="col-md-10">
                             <div>
-                                {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $reply->created_at }}</span>
+                                {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">at {{ $reply->created_at }}</span>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <div>
+                                @if (Auth::id() == $reply->user_id)
+                                    {!! Form::open(['route' => ['replies.destroy', $reply->id], 'method' => 'delete']) !!}
+                                        {!! Form::button('<i class="far fa-trash-alt"></i>', ['type'=> 'submit', 'class' => 'btn btn-link text-secondary']) !!}
+                                    {!! Form::close() !!}
+                                @endif
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-12">
-                            <p>Reply ID: {{ $reply->id }}</p>
-                            <p>Replying to {{ $reply->reply_id }}.</p>
-                            <p>{!! nl2br(e($reply->content)) !!}</p>
-                            <p>{!! nl2br(e($reply->status)) !!}</p>
+                        <div class="col-10 offset-1">
+                            <p class="card-text">{!! nl2br(e($reply->content)) !!}</p>
+                            <p class="card-text">{!! nl2br(e($reply->status)) !!}</p>
+                        </div>
+                        <div class="col-1">
+                            <a data-toggle="collapse" href="#collapsePost{{$reply->id}}" aria-expanded="false" aria-controls="collapseExample">
+                                <i class="fas fa-reply"></i>
+                            </a>
+
                         </div>
                         <div class="col-12">
-                            <div class="row justify-content-end">
-                                @if (Auth::id() == $reply->user_id)
-                                    {!! Form::open(['route' => ['replies.destroy', $reply->id], 'method' => 'delete']) !!}
-                                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                            <div class="collapse" id="collapsePost{{$reply->id}}">
+                                <hr>
+                                <p>You are replying to {{ App\User::find($reply->user_id)->name }}.</p>
+                                
+                                @if (Auth::id() == $user->id)
+                                {!! Form::open(['route' => 'replies.store']) !!}
+                                    <div class="form-group" id="review-form-group">
+                                        {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'id'=>'form-content', 'placeholder'=>'Reply...', 'rows'=>'2']) !!}
+                                        {{ Form::hidden('post_id', $reply->post_id) }}
+                                        {{ Form::hidden('reply_id', $reply->id) }}
+                                        {!! Form::submit('Submit', ['class' => 'btn btn-primary btn-block', 'id' => 'form-button']) !!}
+                                    </div>
                                     {!! Form::close() !!}
                                 @endif
-                                {!! link_to_route('replies.create','Reply', ['reply_id' => $reply->id], ['class'=>'btn btn-primary']) !!}
                             </div>
                         </div>
                     </div>
