85179237e10251a9283c4bf150337070bc6d68ea
maho.tanaka <ec2-user@ip-172-31-37-22.us-east-2.compute.internal>
2018-07-17 04:19:02 +0000

    about

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,86 +1,8 @@
-<?php
-$image = array ('image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg');
-$rand = mt_rand(0,3);
-?>
- 
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css" type="text/css" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+@section('content')
 
-  <title>kashikari</title>
-  <style type="text/css">
-    body{
-      background: linear-gradient(-45deg, rgba(0, 0, 0, .3), rgba(0, 0, 0, .3)), url(images/{{$image[$rand]}});
-      background-position: top center;
-      background-repeat: no-repeat;
-      background-attachment: fixed;
-      background-size: cover;
-      margin-bottom: 0;
-      height:100vh;
-    }
-    
-    .fa-border {
-        color: #D6D1CD;
-    }
-    
-    .fa-hand-holding-heart {
-        color: #E5399D;
-    }
-    
-    .fa-people-carry{
-        color: #3CAFC1;
-    }
-    
-    .fa-comments{
-        color: #E2C73F;
-    }
-    
-    .box1 {
-    background: #e4fcff;/*背景色*/
-    border-top: solid 6px #1dc1d6;
-    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.32);/*影*/
-    }
-    
-    .box2 {
-    background: #F7E8A0;/*背景色*/
-    border-top: solid 6px #F7D736;
-    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.32);/*影*/
-    }
-
-    
-  </style>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-home position-relative">
-    <a class="navbar-brand" href="#"><span class="display-4">KASHIKARI</span></a>
-            @if (Route::has('login'))
-            <ul class="navbar-nav position-absolute">
-                @auth 
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/home') }}">Home</a></li>
-                @else
-                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
-                @endauth
-            </ul>
-            @endif
-    </nav>
-    <div class="jumbotron jumbotron-extend">
-        <div id="logo">
-            <h1 class="display-1 font-weight-bold text-left">KASHIKARI</h1>
-            <p class="display-3 font-weight-bold text-left">You can Borrow</p>
-            <p class="display-3 font-weight-bold text-left">Almost Everything!</p>
-            <p class="lead text-left mt-3">'18 NEW GRADS CODING TRAINING</p>
-        </div>
-    </div>
     <div class="container-fluid">
         <div class="colored content p-5">
             <h2 class="display-2 text-secondary font-weight-bold mb-5">ABOUT</h2>
@@ -231,25 +153,7 @@ $rand = mt_rand(0,3);
         <div class="text-center my-5">
             <a class="button1 shadow-lg" href="{{ route('register') }}">Sign Up Now</a>
         </div>
-        </div>
-        
-        
     </div>
-    
-<footer class="bg-light p-5">
-    <div class="text-center text-muted">©2018 KASHIKARI</div>
-</footer> 
+</div>
 
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-
-
-</body>
-
-
-
-</html>
+@endsection
