7f9744027097b852f17088848ca4af192f88ddc6
maho.tanaka <ec2-user@ip-172-31-37-22.us-east-2.compute.internal>
2018-07-17 05:16:18 +0000

    userpage

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,20 +2,24 @@
 
 @section('content')
     <div class="p-5 bg-dark text-center">
-        <img src="{{ Gravatar::src($user->email, 30) . '&d=mm' }}" alt="" class="img-circle" style="border-radius: 20px;"> 
+        <div class="row mt-5">
+            
+            <div class="col-2 offset-5 px-5">
+                <img src="{{ Gravatar::src($user->email, 300) . '&d=mm' }}" alt="" class="rounded-circle img-fluid" style="border-radius: 20px;"> 
+            </div>
+            @if(Auth::user()->id == $user->id)
+            <div class="col-1 offset-4 text-right">
+                <a href="{{ route('users.edit', Auth::user()->id) }}"><i class="far fa-edit text-light"></i></a>
+            </div>
+            @endif
+        </div>
         <h2 class="font-weight-normal text-light">{{ $user->name }}</h2>
         <p class="lead mt-3 text-light"><i class="fas fa-quote-left text-white-50 mr-3"></i>{{ $user->content }}<i class="fas fa-quote-right text-white-50 ml-3"></i></p>
     </div>
-    <h1 class='text-center'>Your Item List</h1>
-    
+    <h1 class='text-center my-5'>Your Item List</h1>
     <div class="container">
         @include('items.items')
         
-    </div>
-    <div class="container text-center">
-    @if(Auth::user()->id == $user->id)
-        <a href="{{ route('users.edit', Auth::user()->id) }}">Profile Setting</a>
-    @endif
     </div>
 
 @endsection
