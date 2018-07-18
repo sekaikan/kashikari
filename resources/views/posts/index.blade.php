a1acf3b80545e50dd7f4904e65c433cb042cd9d8
erika.tokumoto <>
2018-07-18 03:52:40 +0000

    borrow pages

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,22 +1,29 @@
 @extends('layouts.app')
 
 @section('cover')
-    <div class="jumbotron jumbotron-fluid bg-dark">
+    <div class="jumbotron  bg-dark">
             <h1 class="text-center text-white mt-5">{!! $group->name !!}</h1>
     </div>
 @endsection
 
 @section('content')
+    <div class="container">
+    <div class="row">
+        <div class="col-12 my-3">
+         @include('notifications.notifications')
+        </div>
+    </div>
+    </div>
      <div class="mainmenu status text-center">
        <ul class="nav nav-tabs justify-content-center">
           <li class="nav-item col-4">
             <a class="nav-link" href="/group/{{$group->id}}"><i class="fas fa-home"></i><br>Home</a>
           </li>
           <li class="nav-item col-4">
-            <a class="nav-link" href="/posts/create"><i class="fas fa-sad-tear"></i><br>Borrow</a>
+            <a href="{{ route('posts.borrow', ['id' => $group->id]) }}" class="nav-link"><i class="fas fa-sad-tear"></i><br>Borrow</a>
           </li>
           <li class="nav-item col-4">
-            <a class="nav-link" href="/items/create"><i class="fas fa-smile-wink"></i><br>Lend</a>
+            <a href="{{ route('items.lend', ['id' => $group->id]) }}" class="nav-link"><i class="fas fa-smile-wink"></i><br>Lend</a>
           </li>
        </ul>
     </div>
