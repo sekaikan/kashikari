80ce9796a595eb97632645af7022e00971e5dded
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-19 08:15:49 +0000

    perent 5 added;

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -18,7 +18,7 @@
                                 );
                         ?>
                         
-                        <img class="usericon" src="{{  secure_asset($image_rand[$key]) }}">
+                        <img class="usericon" src="{{  secure_asset($image_rand[$key % 5]) }}">
                         @endforeach
                     @endif
                     <a href="{{route('group.userlist', ['id' => $group->id]) }}" class="lasticon"><i class="fas fa-ellipsis-h text-light"></i></a> 
