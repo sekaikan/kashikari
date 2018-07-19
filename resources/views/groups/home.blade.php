17e203ca9fd7eb00652ea74189c64d7751adb6f1
manato <ec2-user@ip-172-31-11-75.us-east-2.compute.internal>
2018-07-19 07:18:03 +0000

    aa

--- resources/views/groups/home.blade.php
+++ resources/views/groups/home.blade.php
@@ -32,7 +32,7 @@
 
 @section('content')
     <div class="container">
-        <div class="mainmenu status text-center">
+        <div class="mainmenu sen status text-center">
             <ul class="nav nav-tabs justify-content-center">
                 <li class="nav-item active col-4 px-0">
                 <a class="nav-link" href="/group/{{$group->id}}"><i class="fas fa-home"></i><br>Home</a>
