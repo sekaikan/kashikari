b8b7a6a74c59494bf43546ed82e6a5c04f526d64
erika.tokumoto <>
2018-07-23 03:26:33 +0000

    groupsearch2

--- resources/views/groups/create.blade.php
+++ resources/views/groups/create.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 <div class="container my-5">
+    <div class ="row pt-5">
+        <div class="col-md-6 offset-md-3">
+             <h2 class="text-center">Search Other Groups</h2>
+                {!! Form::open(array('method' => 'Get', 'route' => 'groups.search')) !!}
+                 <div class="form-group">
+                    {!! Form::text('keyword', null, ['class' => 'form-control', 'placeholder'=>'Find Groups']) !!}
+                </div>
+                {!! Form::submit('Search', ['class' => 'btn btn-blue btn-lg btn-block']) !!}
+                {!! Form::close() !!}
+        </div>
+    </div>
      <div class ="row pt-5">
         <div class="col-md-6 offset-md-3">
              <h2 class="text-center">Create New Group</h2>
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::group(['middleware' => ['auth']], function () {
 
 //results
    Route::resource('results', 'ResultsController', ['only' =>['index']]);
-   Route::get('results/groupsearch', 'ResultsController@groupsearch')->name('groups.search');
+   Route::get('results/search', 'ResultsController@groupsearch')->name('groups.search');
 
 // notifications
    Route::delete('/', 'NotificationsController@destroy')->name('notifications.destroy');
