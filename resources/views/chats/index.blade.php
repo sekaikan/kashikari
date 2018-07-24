f3e19b2995b8211116542367d5107c0a3fa59313
takumi-hash <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-24 07:31:05 +0000

    chat can be sent with enter key.

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -3,17 +3,13 @@
 @section('content')
 <div class="container my-5">
     <h2 class="text-center pt-5">Group Chat</h2>
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            {!! Form::open(array('route' => array('chats.store', $group->id))) !!}
-                <div class="row form-group">
-                    {{ Form::hidden('group_id', $group->id)}}
-                    {!! Form::textarea('content', NULL, ['class' => 'form-control', 'id' => 'bms_send_message', 'placeholder' => 'What\'s up?', 'rows' =>'3']) !!}
-                    {!! Form::button('<i class="far fa-paper-plane"></i>', ['type'=> 'submit', 'id' => 'bms_send_btn']) !!}
-                </div>
-            {!! Form::close() !!}
-        </div>
-    </div>
+        {!! Form::open(array('route' => array('chats.store', $group->id),'class'=>'col-4 offset-4')) !!}
+            <div class="row">
+            {{ Form::hidden('group_id', $group->id)}}
+            {!! Form::text('content', NULL, ['class' => 'form-control col-10', 'id' => 'bms_send_message', 'placeholder' => 'What\'s up?']) !!}
+            {{ Form::submit('&#xf1d8;', ['class' => 'btn far col-2','id'=>'bms_send_btn']) }}
+            </div>
+        {!! Form::close() !!}
 </div>
 <div class="container">
     @if (count($chats) >0)
