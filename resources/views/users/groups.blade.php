96afbdaa77c6ffbe28e5703c2c75d03b75ddfef3
manato <ec2-user@ip-172-31-11-75.us-east-2.compute.internal>
2018-07-25 03:38:12 +0000

    aa

--- resources/views/users/groups.blade.php
+++ resources/views/users/groups.blade.php
@@ -14,7 +14,7 @@
                 <ul class="nav flex-column nav-pills"  role="tablist" aria-orientation="vertical">
                     <li class="nav-item" class=""><a href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link">ITEMS <span class="badge badge-warning">{{$items->count()}}</span></a></li>
                     <li class="nav-item" class=""><a href="{{ route('users.posts', ['id' => $user->id]) }}"  class="nav-link"> POSTS <span class="badge badge-warning">{{$posts->count()}}</span></a></li>
-                    <li class="nav-item" class="active"><a href="{{ route('users.follows', ['id' => $user->id]) }}" class="nav-link"> GROUPS <span class="badge badge-warning">{{$follow_groups->count()}}</span></a></li>
+                    <li class="nav-item active" class=""><a href="{{ route('users.follows', ['id' => $user->id]) }}" class="nav-link"> GROUPS <span class="badge badge-warning">{{$follow_groups->count()}}</span></a></li>
                 </ul>
 
             </div>
