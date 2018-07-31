2b510be1c1c47b3bace85b3dab114219ffed3045
erika.tokumoto <>
2018-07-31 01:56:20 +0000

    tab added

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -1,7 +1,45 @@
 @extends('layouts.app')
 
+
+@section('cover')
+     <div class="jumbotron jumbotron-home">
+            <h1 class="text-center text-white mt-5">{!! $group->name !!}</h1>
+            
+            <div class="col-3 mx-auto">
+                <div class="mx-auto">
+                    @if (isset($groupusers))
+                        @foreach($groupusers as $key => $groupuser)
+                            <img class="usericon" src="{{ $groupuser->photo }}">
+                        @endforeach
+                    @endif
+                    <a href="{{route('group.userlist', ['id' => $group->id]) }}" class="lasticon"><i class="fas fa-ellipsis-h text-light"></i></a> 
+                </div>
+            </div>
+            
+    </div>
+@endsection
+
 @section('content')
-<div class="container my-5">
+<div class="container">
+    <div class="mainmenu sen status text-center mt-2">
+       <ul class="nav nav-tabs justify-content-center">
+          <li class="nav-item col-3 px-0">
+            <a class="nav-link" href="/group/{{$group->id}}"><i class="fas fa-home"></i><br>Home</a>
+          </li>
+          <li class="nav-item col-3 px-0">
+            <a href="{{ route('posts.borrow', ['id' => $group->id]) }}" class="nav-link"><i class="fas fa-hand-holding-heart"></i><br>Borrow</a>
+          </li>
+          <li class="nav-item col-3 px-0">
+            <a href="{{ route('items.lend', ['id' => $group->id]) }}" class="nav-link"><i class="fas fa-people-carry"></i><br>Lend</a>
+          </li>
+          <li class="nav-item col-3 px-0 active">
+            <a href="{{ route('chats.index', ['id' => $group->id]) }}" class="nav-link"><i class="fas fa-comments"></i><br>Chats</a>
+          </li>
+       </ul>
+    </div>
+
+    
+<div class="container bg-light">
     <h2 class="text-center pt-5">Group Chat</h2>
         {!! Form::open(array('route' => array('chats.store', $group->id),'class'=>'col-4 offset-4')) !!}
             <div class="row">
@@ -10,10 +48,9 @@
             {{ Form::submit('&#xf1d8;', ['class' => 'btn far col-2','id'=>'bms_send_btn']) }}
             </div>
         {!! Form::close() !!}
-</div>
-<div class="container">
     @if (count($chats) >0)
         @include('chats.chats')
     @endif
+</div>
 </div>
 @endsection
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,58 +1,63 @@
     <nav class="navbar navbar-expand-lg navbar-extend fixed-top" style="background-color:#24292E;" >
         <div class="container">
             <div class="navbar-header">
+                <?php $url = $_SERVER['REQUEST_URI'];?>
                 @if (Auth::check())
-                <a class="navbar-brand navbar-left text-light" href="/home">Kashikari</a>
+                    @if(strstr($url,'search') || strstr($url,'create'))
+                    <a class="navbar-brand navbar-left text-light" href="/home">Kashikari</a>
+                    @elseif(strstr($url,'group'))
+                      <a class="navbar-brand navbar-left  text-light" href="{{route('group.show', ['id' => $group->id])}}">Kashikari <span class="navbar-brand text-muted">|</span>{!! $group->name !!}</a>
+                    @else
+                      <a class="navbar-brand navbar-left text-light" href="/home">Kashikari</a>
+                    @endif
+                    
                 @else
                 <a class="navbar-brand navbar-left  text-light" href="/">Kashikari</a>
-                @endif
-                <?php $url = $_SERVER['REQUEST_URI'];?>
-                @if(strstr($url,'search') || strstr($url,'create'))
-                
-                @elseif(strstr($url,'group'))
-                  <span class="navbar-brand text-muted">|</span>
-                  <a class="navbar-brand navbar-left  text-light" href="{{route('group.show', ['id' => $group->id])}}">{!! $group->name !!}</a>
                 @endif
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
+            <?php $url = $_SERVER['REQUEST_URI'];?>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
+                    @if(strstr($url,'home'))
+                    @else
+                    <a class="nav-link  text-light" href="{{route('home')}}">GroupList</a>
+                    @endif
                     <a class="nav-link  text-light" href="{{route('about')}}">About Us</a>
                     @if (Auth::check())
-                        <?php $url = $_SERVER['REQUEST_URI'];?>
-                        @if(strstr($url,'about'))
-                        @elseif(strstr($url,'search') || strstr($url,'home'))
-                        <div class="nav-link">
-                            <a data-toggle="collapse" href="#navbar-search" aria-expanded="false" aria-controls="collapseExample">
-                                <i class="fas fa-search  text-light"></i>
-                            </a>
-                        </div>
-                        <div class="collapse width" id="navbar-search">
-                                <form class="form-inline" action="{{url('/results/search')}}">
-                                    <div class="form-group mr-2 bg-light">
-                                        <input type="text" name="keyword" value="" class="form-control bg-transparent" placeholder="Find Groups">
-                                    </div>
-                                    <!--input type="submit" value="Search" class="btn btn-outline-success"-->
-                                </form>
-                        </div>
+                        @if(strstr($url,'about')|| strstr($url,'home'))
+                        @elseif(strstr($url,'search'))
+                            <div class="nav-link">
+                                <a data-toggle="collapse" href="#navbar-search" aria-expanded="false" aria-controls="collapseExample">
+                                    <i class="fas fa-search  text-light"></i>
+                                </a>
+                            </div>
+                            <div class="collapse width" id="navbar-search">
+                                    <form class="form-inline" action="{{url('/results/search')}}">
+                                        <div class="form-group mr-2 bg-light">
+                                            <input type="text" name="keyword" value="" class="form-control bg-transparent" placeholder="Find Groups">
+                                        </div>
+                                        <!--input type="submit" value="Search" class="btn btn-outline-success"-->
+                                    </form>
+                            </div>
                         
-                    @else
-                        <div class="nav-link">
-                            <a data-toggle="collapse" href="#navbar-search" aria-expanded="false" aria-controls="collapseExample">
-                                <i class="fas fa-search  text-light"></i>
-                            </a>
-                        </div>
-                        <div class="collapse width" id="navbar-search">
-                                <form class="form-inline" action="{{url('/results/')}}">
-                                    <div class="form-group mr-2 bg-light">
-                                        <input type="text" name="keyword" value="" class="form-control bg-transparent" placeholder="Find Items">
-                                    </div>
-                                    <!--input type="submit" value="Search" class="btn btn-outline-success"-->
-                                </form>
-                        </div>
-                    @endif
+                        @else
+                            <div class="nav-link">
+                                <a data-toggle="collapse" href="#navbar-search" aria-expanded="false" aria-controls="collapseExample">
+                                    <i class="fas fa-search  text-light"></i>
+                                </a>
+                            </div>
+                            <div class="collapse width" id="navbar-search">
+                                    <form class="form-inline" action="{{url('/results/')}}">
+                                        <div class="form-group mr-2 bg-light">
+                                            <input type="text" name="keyword" value="" class="form-control bg-transparent" placeholder="Find Items">
+                                        </div>
+                                        <!--input type="submit" value="Search" class="btn btn-outline-success"-->
+                                    </form>
+                            </div>
+                        @endif
                         <?php $notifications = \DB::table('notifications')->where('recipient_id', \Auth::id())->orderBy('created_at', 'desc')->paginate(5); ?>
                        @if($notifications->count()==0)
                        <a tabindex="0" class="navbar-item btn btn-link text-dark" role="button" data-toggle="popover" data-trigger="focus" data-placement="bottom" data-html="true" title="Notifications" 
